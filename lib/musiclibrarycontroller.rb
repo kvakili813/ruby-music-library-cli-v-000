243a1b5aa178b7a667a09df726fb59a6fcb54134
Kayvan Vakili <kvakili813@gmail.com>
2018-06-22 16:17:39 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -19,6 +19,9 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       name = gets.strip
+      if name == "list songs"
+        list_songs
+      end
     end
   end
 
